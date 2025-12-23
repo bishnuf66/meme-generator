2d3ae0b6e3120edd92249661d94a9ae116945293
bishnuf66 <bishowkarmabishnu2024@gmail.com>
2025-12-23 22:06:03 +0545

    added vercel analytic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./component/Header";
 import Meme from "./component/Meme";
+import { Analytics } from "@vercel/analytics/react";
 
 function App(): JSX.Element {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100">
       <Header />
       <Meme />
+      <Analytics />
     </div>
   );
 }
